44f2a1d66a209b98b2b729d261abd6d1485a9019
ShamimHossain1 <beastb697@gmail.com>
2023-02-27 15:31:44 +0600

    methods

---
+++ methods.js
@@ -0,0 +1,17 @@
+const student = {
+    name: 'shamim',
+    age:20,
+    study:'math',
+    subject:['english', 'physics', 'chemistry'],
+    exam: function(){
+        // console.log(this.name, 'is participating in an exm');
+    },
+    improveExm: function(subject){
+        this.exam();
+        return `${this.name} is takeing improvement exam on ${subject}`
+    }
+}
+
+// student.exam();
+const improveExm = student.improveExm(student.subject[1]);
+// console.log(improveExm);
